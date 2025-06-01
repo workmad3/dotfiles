46bd6a6ba8d18183aa6ae11337ebf9d5f66de861
David Workman <workmad3@Davids-MacBook-Pro.local>
2025-06-01 21:27:45 +0100

    Updating mise to allow local and brew installs. Removing multiple op accounts in profile

--- terminal/zshrc.d/08-mise.zsh
+++ terminal/zshrc.d/08-mise.zsh
@@ -1,1 +1,5 @@
-eval "$(/Users/workmad3/.local/bin/mise activate zsh)"
+if [ -f "/Users/workmad3/.local/bin/mise" ]; then
+	eval "$(/Users/workmad3/.local/bin/mise activate zsh)"
+else
+	eval "$(mise activate zsh)"
+fi
