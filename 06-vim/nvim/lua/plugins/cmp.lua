a07b3815403428e254f1f402d858b3a20ed2fc30
David Workman <david.workman@carwow.co.uk>
2025-06-06 10:40:01 +0100

    Adding nvim-cmp plugins and updating installed plugins

---
+++ 06-vim/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+		  require("config.nvim-cmp")
+		end,
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets"
+		}
+	}
+}
