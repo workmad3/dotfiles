605fe679ff32e2eafd90d6e13e36c5d4ea5d4c02
David Workman <me@workmad3.com>
2025-06-08 20:55:07 +0100

    Change to tokyonight theme

--- 06-vim/nvim/lua/plugins/init.lua
+++ 06-vim/nvim/lua/plugins/init.lua
@@ -1,12 +1,12 @@
 return {
 	{
-		"flazz/vim-colorschemes",
+		"folke/tokyonight.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
 			vim.g.background = 'dark'
-			vim.cmd.colorscheme('Tomorrow-Night')
+			vim.cmd.colorscheme('tokyonight')
 		end,
 	},
 }
