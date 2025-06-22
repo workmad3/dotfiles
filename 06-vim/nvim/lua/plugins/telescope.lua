2485cb0606f6314badbe2da3c8caa23244d48b62
David Workman <me@workmad3.com>
2025-06-22 23:27:25 +0100

    Mark these as lazy

--- 06-vim/nvim/lua/plugins/telescope.lua
+++ 06-vim/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
+		lazy = true,
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-lua/plenary.nvim",
@@ -10,6 +11,7 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = true,
 		event = { "BufReadPost", "BufNewFile" },
 		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
 		build = ":TSUpdate",
